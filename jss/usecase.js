e6f45b8724d4753f001f9f454577dfeca7415441
chromko <mankam03@web.de>
2024-07-19 16:12:13 +0200

    Test Audio

--- jss/usecase.js
+++ jss/usecase.js
@@ -196,7 +196,11 @@ function initializeWebAudio(poi) {
     // get soundfile from rest api call and get audio element of poi
     const audioUrl = `${SERVER_URL}/soundfiles/${poi.soundfile_id}`;
     fetch(audioUrl)
-        .catch(error => alert(error));
+        .then(response => {
+          if (response.status === 404) {
+              alert("Test")
+          }
+        });
     const audioElement = new Audio(audioUrl);
     audioElements[poi.order] = audioElement;
 
