7485c41262622edd3b00c7e7c177195ba5253fd6
chromko <mankam03@web.de>
2024-07-19 17:20:56 +0200

    Test IOS Fix

--- jss/usecase.js
+++ jss/usecase.js
@@ -74,7 +74,7 @@ function showPopup() {
  */
 function submitUseCaseId() {
 
-    let test = new Audio('jss/silence.mp3');
+    let test = new Audio(`${SERVER_URL}/soundfiles/2`);
     test.play();
 
     // check if usecase id is given. if not, show popup again
